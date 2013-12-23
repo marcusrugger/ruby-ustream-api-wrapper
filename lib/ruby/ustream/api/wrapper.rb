493d5f93abee230919cef653cc51701b27b0355d
Marcus Rugger <marcusrugger@yahoo.com>
2013-12-23 12:25:43 -0500

    Initial creation of the gem

---
+++ lib/ruby/ustream/api/wrapper.rb
@@ -0,0 +1,11 @@
+require "ruby/ustream/api/wrapper/version"
+
+module Ruby
+  module Ustream
+    module Api
+      module Wrapper
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/ruby/ustream/api/wrapper/version.rb
@@ -0,0 +1,9 @@
+module Ruby
+  module Ustream
+    module Api
+      module Wrapper
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
